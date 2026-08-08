491a902d4813fd962cb3c3838a969af71b3d02f7
kiskey <59226371+kiskey@users.noreply.github.com>
2026-08-07 23:26:31 -0700

    Remove unused FontWeight import from DevicesScreen.kt

--- app/src/main/java/com/lias/remote/ui/screens/devices/DevicesScreen.kt
+++ app/src/main/java/com/lias/remote/ui/screens/devices/DevicesScreen.kt
@@ -24,7 +24,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.lias.remote.core.models.Device
 import com.lias.remote.core.models.Tag
